49944d2e7562965a62a79668f0e67b32988e3493
SantaKoska <kamalsankarm@gmail.com>
2024-08-17 13:18:41 +0530

    initial

--- artistry-hub-backend/index.js
+++ artistry-hub-backend/index.js
@@ -6,6 +6,9 @@ app.get("/", (req, res) => {
     res.send("I am Working");
 });
 
+app.get("/hello", (req, res) => {
+    res.send("hello World, This is a new route");
+})
 app.listen(8000, () => {
     console.log("Sercer is running")
 });
