0ebeddc05d0547f4c271a84b20ff7c2ab17ecde8
SantaKoska <kamalsankarm@gmail.com>
2025-02-18 13:07:18 +0530

    Remove live classes feature from backend and frontend

--- artistry-hub-backend/index.js
+++ artistry-hub-backend/index.js
@@ -19,7 +19,6 @@ const comment = require("./routes/comments");
 const instrumentservice = require("./routes/instrumentServiceAssistant");
 const events = require("./routes/events");
 const jobs = require("./routes/jobs");
-const liveClassRoutes = require("./routes/liveClass");
 
 //for env
 require("dotenv").config();
@@ -137,8 +136,6 @@ app.use("/instrumentservice", instrumentservice);
 app.use("/events", events);
 
 app.use("/jobs", jobs);
-
-app.use("/live-classes", liveClassRoutes);
 
 //the app losening to the port
 app.listen(8000, () => {
